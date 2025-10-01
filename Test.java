4566a10377a70ff5ea8273289cf72e1f6b8964f5
naufalazmiaqil <incoldtown@gmail.com>
2025-10-01 14:31:44 +0700

    p

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
-        System.out.println("nopal adrian");
+        System.out.println("nopal adriann");
     }
 }
